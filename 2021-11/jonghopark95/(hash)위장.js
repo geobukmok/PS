145f23eb282d7ae02c13ecfae855c796b4c5f932
박종호 <jong951005@gmail.com>
2021-11-24 21:28:31 +0900

    Update (hash)위장.js

--- 2021-11/jonghopark95/(hash)위장.js
+++ 2021-11/jonghopark95/(hash)위장.js
@@ -1,14 +1,14 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42578
 
 function solution(clothes) {
-  const clothHash = {};
+  const clothHash = new Map();
   const clothAmount = [];
   let clothAmountKey = 0;
 
   clothes.map(([_, kind]) => {
-    if (!(kind in clothHash)) clothHash[kind] = clothAmountKey++;
+    if (!clothHash.has(kind)) clothHash.set(kind, clothAmountKey++);
 
-    const key = clothHash[kind];
+    const key = clothHash.get(kind);
     clothAmount[key] = (clothAmount[key] || 0) + 1;
   });
 
