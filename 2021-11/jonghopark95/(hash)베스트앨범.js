e709c8c5ac298213c5770fe60a331dff827d0e16
박종호 <jong951005@gmail.com>
2021-11-26 12:12:00 +0900

    Create (hash)베스트앨범.js

---
+++ 2021-11/jonghopark95/(hash)베스트앨범.js
@@ -0,0 +1,32 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42579?language=javascript
+
+function solution(genres, plays) {
+  const genreMap = new Map();
+  const amountMap = new Map();
+  let answer = [];
+
+  genres.forEach((genre, idx) => {
+    const playInfo = [idx, plays[idx]];
+
+    if (!genreMap.has(genre)) {
+      genreMap.set(genre, [playInfo]);
+    } else {
+      genreMap.set(genre, [...genreMap.get(genre), playInfo]);
+    }
+
+    amountMap.set(genre, (amountMap.get(genre) || 0) + plays[idx]);
+  });
+
+  const amount = [...amountMap].sort((l, r) => r[1] - l[1]);
+  amount.forEach(([genre]) => {
+    const bestOnGenre = genreMap
+      .get(genre)
+      .sort((l, r) => r[1] - l[1])
+      .slice(0, 2)
+      .map(([num]) => num);
+
+    answer = [...answer, ...bestOnGenre];
+  });
+
+  return answer;
+}
