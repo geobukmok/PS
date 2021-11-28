5432134e43e9e9488396b0ed6f3792021dd56d78
HanCh <05.hanch.it@gmail.com>
2021-11-28 19:00:57 +0900

    카펫 수정

--- 2021-11/HanCheo/12. 카펫.js
+++ 2021-11/HanCheo/12. 카펫.js
@@ -1,5 +1,5 @@
 function solution(brown, yellow) {
-  for (let i = 1; i <= yellow; i++) {
+  for (let i = 1; i <= yellow/2+1; i++) {
     if ((i + 2) * 2 + (yellow / i) * 2 === brown) 
 			return [i + 2, yellow / i + 2].sort((a, b) => b - a);
   }
