c977425c6e45d6509a8a68d581d5e9283b96ac39
HanCh <05.hanch.it@gmail.com>
2021-11-28 20:10:34 +0900

    소수찾기 추가

---
+++ 2021-11/HanCheo/13. 소수찾기.js
@@ -0,0 +1,36 @@
+function solution(numbers) {
+  let answer = 0;
+  const numArr = numbers
+    .split('')
+    .map(Number);
+  const checked = new Map();
+
+  for (let i = 0; i < numArr.length; i++) {
+    const nextArr = [...numArr.slice(0, i), ...numArr.slice(i + 1)];
+    DFS(`${numArr[i]}`, nextArr);
+  }
+
+  function DFS(num, numsArr) {
+    const checkNum = Number(num);
+    if (!checked.get(checkNum) && isSosu(checkNum)) answer++;
+    checked.set(checkNum, true);
+
+    for (let i = 0; i < numsArr.length; i++) {
+      const nextArr = [...numsArr.slice(0, i), ...numsArr.slice(i + 1)];
+      DFS(`${num}${numsArr[i]}`, nextArr);
+    }
+  }
+
+  return answer;
+}
+
+function isSosu(num) {
+  if (num <= 1) return false;
+  const max = Math.sqrt(num);
+
+  for (let i = 2; i <= max; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
