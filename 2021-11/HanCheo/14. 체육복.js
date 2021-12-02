5ee1a12fa17fb97cd261fedb17408ab9980992c8
HanCh <05.hanch.it@gmail.com>
2021-12-02 11:18:20 +0900

    Update 14. 체육복.js

--- 2021-11/HanCheo/14. 체육복.js
+++ 2021-11/HanCheo/14. 체육복.js
@@ -1,15 +1,14 @@
 function solution(n, lost, reserve) {
-  let answer = n;
   const reserveSet = new Set(reserve);
 
   return (
-    answer -
+    n -
     lost
       .filter((stu) => !reserveSet.delete(stu))
       .sort((a, b) => a - b)
       .filter((stu) => (
-				reserveSet.delete(stu - 1) || 
-				reserveSet.delete(stu + 1) ? false : true))
-			.length
+	reserveSet.delete(stu - 1) || 
+	reserveSet.delete(stu + 1) ? false : true))
+      .length
   );
 }
