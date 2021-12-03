42c1a118c2ad2a1a92d4b3bedaf29ae75082b14c
HanCh <05.hanch.it@gmail.com>
2021-12-03 16:25:47 +0900

    [hancheo]: 큰수만들기 추가

---
+++ 2021-11/HanCheo/17. 큰수만들기.js
@@ -0,0 +1,27 @@
+function solution(number, k) {
+  let numbers = number.split('');
+	const answer = [numbers[0]];
+
+  numbers.reduce((a, b) => {
+    if (a < b && k > 0) {
+      while (k > 0) {
+        const popNum = answer[answer.length - 1];
+        if (popNum < b) {
+          answer.pop();
+          k--;
+        } else {
+          break;
+        }
+      }
+    }
+    answer.push(b);
+    return b;
+  });
+
+  while (k > 0) {
+    answer.pop();
+    k--;
+  }
+
+  return answer.join('');
+}
