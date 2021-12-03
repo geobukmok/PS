06d3795a7c474748018965147be51aaefd24d120
chaeeun037 <chaeeun.dev@gmail.com>
2021-12-03 15:18:10 +0900

    fix: 구명보트 수정

--- 2021-11/chaeeun037/lifeboat.js
+++ 2021-11/chaeeun037/lifeboat.js
@@ -1,9 +1,11 @@
 function solution(people, limit) {
     let answer = people.length;
-    let arr = people.slice().sort((a, b) => b - a);
-    let left = 0, right = arr.length - 1;
+    let left = 0, right = people.length - 1;
+    
+    people.sort((a, b) => b - a);
+    
     while(left < right) {
-        if(arr[left] + arr[right] <= limit) {
+        if(people[left] + people[right] <= limit) {
             left++;
             right--;
             answer--;
@@ -11,5 +13,6 @@ function solution(people, limit) {
             left++;
         }
     }
+    
     return answer;
 }
